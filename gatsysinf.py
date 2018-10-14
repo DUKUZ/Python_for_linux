95571a34ab0102a9445cd707e1f4020f71d02bda
Denis Konieiev <d.konieiev@gmail.com>
2018-10-14 10:57:53 +0300

    added python file

---
+++ gatsysinf.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# Gathering system information script
+import subprocess
+
+#Command 1
+def uname_func():
+
+	uname = "uname"
+	uname_arg = "-a"
+	print "Gathering system information with %s command:\n" % uname
+	subprocess.call([uname, uname_arg])
+
+#Command 2
+def disk_func():
+
+	diskspace = "df"
+	diskspace_arg = "-h"
+	print "Gathering diskspace information %s command:\n" % diskspace
+	subprocess.call([diskspace, diskspace_arg])
+
+#Main function, what call other functions
+def main():
+	uname_func()
+	disk_func()
+
+if __name__ == "__main__":
+	main()
+
+
